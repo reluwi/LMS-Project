dfadddd0c2d9f2fb89281cc296460d3d30557bb0
Renzo Osmillo <osmillo08@gmail.com>
2025-07-17 13:20:35 +0800

    successful connection to db

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
+
+connectDB();
 
 const port = process.env.PORT || 5000;
 
